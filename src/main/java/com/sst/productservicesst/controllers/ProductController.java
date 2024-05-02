69096ab541ddf354cc2ba9b6e06fed460d03c9fa
Arjun Kshirsagar <146163103+arjun-kshirsagar@users.noreply.github.com>
2024-05-02 06:32:18 +0530

    Update ProductController.java

--- src/main/java/com/sst/productservicesst/controllers/ProductController.java
+++ src/main/java/com/sst/productservicesst/controllers/ProductController.java
@@ -32,7 +32,6 @@ public class ProductController { // waiter
 //        try {
 //            product = productService.getProductById(id);
 //            responseEntity = new ResponseEntity<>(product, HttpStatus.OK);
-//            System.out.println("Hello");
 //            return responseEntity;
 //        } catch (RuntimeException exception) {
 //            ExceptionDto dto = new ExceptionDto();
